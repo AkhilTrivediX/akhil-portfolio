4cecfc1801cbdb9f0f2547234def9253388ce96e
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2025-02-27 14:04:32 +0530

    Fixed middleware (why is it so hard?)

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest){
-    const host = req.headers.get('host') || '';
+    const host = req.headers.get('x-forwarded-host') || req.nextUrl.host;
+    const { pathname} = req.nextUrl;
 
-    console.log('Hosts', host)
 
     if (req.nextUrl.pathname.startsWith('/_next') || req.nextUrl.pathname.startsWith('/static')) {
 
         return NextResponse.next();
     }
 
-    if(host.includes('vinima')){
-        return NextResponse.rewrite(new URL(`/vinima`, req.url));
+    const subdomainMatch = host.match(/^(?:https?:\/\/)?(?:www\.)?([^\.]+)\.(?:[\w-]+\.\w+|localhost)(?::\d+)?$/);
+    const subdomain = subdomainMatch && subdomainMatch[1]?subdomainMatch[1]:''
+    console.log('URL:', host)
+    console.log('Subdomain', subdomain)
+
+    if(subdomain){
+        const targetUrl = new URL('/'+subdomain+pathname, req.url)
+        console.log('Targeting to url:',targetUrl)
+        return NextResponse.rewrite(targetUrl);
     }
 
     return NextResponse.next();
