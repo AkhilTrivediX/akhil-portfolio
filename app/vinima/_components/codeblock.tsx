12708e953a39532207aa3b739475bf16d7aaf5c5
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2025-04-27 09:04:53 +0530

    codeblock update

--- app/vinima/_components/codeblock.tsx
+++ app/vinima/_components/codeblock.tsx
@@ -7,24 +7,34 @@ import { FaRegClipboard } from "react-icons/fa6";
 import { IoCheckmarkSharp } from "react-icons/io5";
 import {motion, AnimatePresence} from 'motion/react'
 import { useState } from 'react';
+import SwitchTabs from './natives/switchTabs';
 
-export default function Codeblock({code, language='tsx'}:{code: string, language?:string})
+export default function Codeblock({code, language='tsx'}:{code: string | {[key:string]:string}, language?:string})
 {
     const [codeCopied, setCodeCopied] = useState(false);
     const customLanguages = ['']
+    const [currentBlock, setCurrentBlock] = useState(typeof code =='object'?Object.keys(code)[0]:'');
     return(
-                    <>
+                    <div className='flex flex-col w-full'>
+                <div className='flex items-center justify-between'>
+                    <SwitchTabs tabs={[{id:'pnpm'},{id:'npm'},{id:'yarn'},{id:'bun'}]} currentTab={currentBlock} setCurrentTab={setCurrentBlock} className='pt-2'/>
+                    <div className="bg-background/80 rounded-md p-2 opacity-80 border-input border cursor-pointer" onClick={()=>{if(codeCopied) return;navigator.clipboard.writeText(code); setCodeCopied(true); setTimeout(()=>setCodeCopied(false), 1000)}}>
+                        <AnimatePresence mode="popLayout">
+                            ${codeCopied?<motion.div key={'checkmark'} initial={{opacity: 0, y: 10}} animate={{opacity:1, y: 0}} exit={{opacity: 0, y: -10}}><IoCheckmarkSharp /></motion.div>:<motion.div key={'clipboard'} initial={{opacity: 0, y: 10}} animate={{opacity:1, y: 0}} exit={{opacity: 0, y: -10}}><FaRegClipboard /></motion.div>}
+                        </AnimatePresence>
+                    </div>
+                </div>
                 {!customLanguages.includes(language)?(<ReactMarkdown className={'markdown w-full h-full overflow-y-scroll no-scrollbars'}
                      rehypePlugins={[rehypeHighlight]}
                     >{`\`\`\`${language || 'tsx'}
 ${code}
 \`\`\`
             `}</ReactMarkdown>):(<code className='flex gap-2'>{code.split(' ').map((token,i)=><span key={i} className={customClass(token, language)}>{token}</span>)}</code>)}
-            <div className="absolute right-4 top-4 bg-background/80 rounded-md p-2 opacity-80 border-input border cursor-pointer" onClick={()=>{if(codeCopied) return;navigator.clipboard.writeText(code); setCodeCopied(true); setTimeout(()=>setCodeCopied(false), 1000)}}>
+            {typeof code=='string'?<div className="absolute right-4 top-4 bg-background/80 rounded-md p-2 opacity-80 border-input border cursor-pointer" onClick={()=>{if(codeCopied) return;navigator.clipboard.writeText(code); setCodeCopied(true); setTimeout(()=>setCodeCopied(false), 1000)}}>
                 <AnimatePresence mode="popLayout">
                     ${codeCopied?<motion.div key={'checkmark'} initial={{opacity: 0, y: 10}} animate={{opacity:1, y: 0}} exit={{opacity: 0, y: -10}}><IoCheckmarkSharp /></motion.div>:<motion.div key={'clipboard'} initial={{opacity: 0, y: 10}} animate={{opacity:1, y: 0}} exit={{opacity: 0, y: -10}}><FaRegClipboard /></motion.div>}
                 </AnimatePresence>
-            </div></>
+            </div>:null}</div>
     )
 }
 
--- app/vinima/_components/natives/switchTabs.tsx
+++ app/vinima/_components/natives/switchTabs.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { LayoutGroup, motion } from 'motion/react'
-export default function SwitchTabs({tabs,currentTab, setCurrentTab}:{tabs:{id:string, name: string}[],currentTab: string, setCurrentTab: React.Dispatch<React.SetStateAction<string>>}){
+export default function SwitchTabs({tabs,currentTab, setCurrentTab, className}:{tabs:{id:string, name?: string}[],currentTab: string, setCurrentTab: React.Dispatch<React.SetStateAction<string>>,className?:string}){
     return (
         <LayoutGroup id={crypto.randomUUID()}>
-            <div className="inline-flex h-9 items-center text-muted-foreground w-full justify-start rounded-none border-b bg-transparent p-0">
+            <div className={"inline-flex h-9 items-center text-muted-foreground w-full justify-start rounded-none border-b bg-transparent p-0 "+className}>
                 {tabs.map((tab)=><Button key={tab.id} variant={'ghost'} className="hover:bg-transparent hover:text-foreground/80 relative pb-4" onClick={()=>setCurrentTab(tab.id)}>
-                    <span className={currentTab===tab.id?'text-foreground':''}>{tab.name}</span>
+                    <span className={currentTab===tab.id?'text-foreground':''}>{tab.name || tab.id}</span>
                     {currentTab === tab.id && <motion.div layoutId='tabUnderline' className="absolute bottom-0 left-0 right-0 h-0.5 bg-foreground"/>}
                 </Button>)}
             </div>
